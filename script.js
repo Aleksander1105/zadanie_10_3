bbda588d2b47dfd82c4550a6e9c728d094786c02
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-03-08 00:28:15 +0100

    Working on the project

---
+++ script.js
@@ -0,0 +1,7 @@
+$(function(){});
+var carouselList = $("#carousel ul");
+carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+var firstItem = carouselList.find("li:first");
+var lastItem = carouselList.find("li:last");
+lastItem.after(firstItem)
+carouselList.css({marginLeft:0});
