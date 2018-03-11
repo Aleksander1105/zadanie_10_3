637006c8da6fccc3d813f66972d66941ae5993db
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-03-11 14:19:44 +0100

    JS does not work

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-$(function(){});
-var carouselList = $("#carousel ul");
-carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-var firstItem = carouselList.find("li:first");
-var lastItem = carouselList.find("li:last");
-lastItem.after(firstItem)
-carouselList.css({marginLeft:0});
+$(function(){
+	setTimeout(changeSlide() {
+		var carouselList = $("#carousel ul");
+		carouselList.animate({'marginLeft':-400}, 500, changeSlide(){
+			var firstItem = carouselList.find("li:first");
+			var lastItem = carouselList.find("li:last");
+			lastItem.after(firstItem);
+			carouselList.css({marginLeft:0});
+		});
+	}, 3000);
+});
