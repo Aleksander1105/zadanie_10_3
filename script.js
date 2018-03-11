ad795f6bd5c5cfd736cd59a32ff8bb42c95bf359
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-03-11 17:15:37 +0100

    Working on the project

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 $(function(){
-	setTimeout(changeSlide() {
+
+	let moveSlide;
+	
+	function moveCarousel() {
+		
 		var carouselList = $("#carousel ul");
-		carouselList.animate({'marginLeft':-400}, 500, changeSlide(){
-			var firstItem = carouselList.find("li:first");
-			var lastItem = carouselList.find("li:last");
-			lastItem.after(firstItem);
-			carouselList.css({marginLeft:0});
+
+		moveSlide = setInterval(function() {
+			carouselList.animate({'marginLeft':-400}, 500, function(){
+				var firstItem = carouselList.find("li:first");
+				var lastItem = carouselList.find("li:last");
+				lastItem.after(firstItem);
+				carouselList.css({marginLeft:0});
+			}); 
+		}, 3000);
+	}
+
+	function stop() {
+    	clearInterval(moveCarousel);
+  	}
+    
+    moveCarousel();
+
+    $("#carousel").mouseenter(stop);
+  	$("#carousel").mouseleave(moveSlide);
+
+
+    $('#btn-right').on('click', function() {
+    	carouselList.animate({'marginLeft':-400}, 500, function(){
+				var firstItem = carouselList.find("li:first");
+				var lastItem = carouselList.find("li:last");
+				lastItem.after(firstItem);
+				carouselList.css({marginLeft:0});
 		});
-	}, 3000);
+    });
+
+    $('#btn-left').on('click', function() {
+    	carouselList.animate({'marginLeft': 0}, 500, function(){
+				var firstItem = carouselList.find("li:first");
+				var lastItem = carouselList.find("li:last");
+				lastItem.after(firstItem);
+				carouselList.css({marginLeft:-400});
+		});
+    });
 });
+
